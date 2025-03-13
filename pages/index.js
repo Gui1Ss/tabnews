3160faf8db2dd073b6215b69aba31bf422d32db6
Guilherme <guilherme.souto063@gmail.com>
2025-03-13 17:12:08 -0300

    adicionando script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 function Home() {
-    return <h1>mensagem</h1>    
+  return <h1>mensagem</h1>;
 }
-  
+
+function teste() {
+  console.log("hsfujwhfuw");
+  ddwqdqw;
+}
+
 export default Home;
 
-//git add 
-//git status 
-//git commit -m "texto"                                   
-//git push 
+//git add
+//git status
+//git commit -m "texto"
+//git push ;
